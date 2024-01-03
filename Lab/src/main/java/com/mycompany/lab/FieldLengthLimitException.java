2a95a38ceb27be3947dc3f8346afd653b801a802
delingar <delingaryt@gmail.com>
2024-01-03 23:48:09 +0200

    Revert "Лабораторна №1"
    This reverts commit 27702108ba7ad2d6a550df7486fbfdfc3981a62f.

--- Lab/src/main/java/com/mycompany/lab/FieldLengthLimitException.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.lab;
-
-public class FieldLengthLimitException extends Exception{
-    public FieldLengthLimitException() {
-    }
-    public FieldLengthLimitException(String msg) {
-        super(msg);
-    }
-}
